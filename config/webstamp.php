bb359c8f7a63a35a9064c10436f710f6bb90bf68
Thomas Lohner <thomas@stormbolt.ch>
2023-04-17 14:29:18 +0200

    first commit

---
+++ config/webstamp.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'franking_license' => env('WS_BARCODE_FRANKING_LICENSE'),
+    'technical_user' => env('WS_BARCODE_TECHNICAL_USER'),
+    'technical_password' => env('WS_BARCODE_TECHNICAL_PASSWORD'),
+    'client_id' => env('WS_BARCODE_CLIENT_ID'),
+    'client_secret' => env('WS_BARCODE_CLIENT_SECRET'),
+    'url' => env('WS_BARCODE_URL'),
+    'debug' => env('WS_BARCODE_DEBUG')
+];
---
+++ src/ServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Asendari\WebServiceWebstamp;
+
+use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
+
+
+class ServiceProvider extends IlluminateServiceProvider {
+
+    public function register(): void
+    {}
+
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__.'/../config/barcode.php' => config_path('webstamp.php'),
+        ], 'config');
+    }
+}
